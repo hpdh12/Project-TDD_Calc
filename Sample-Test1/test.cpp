2720421f56420d49e2f022f587d4554fd9306291
hr_kang <hyerim513@gmail.com>
2023-07-05 09:35:37 +0900

    add new test case for getZegop

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
+#include "../TDD_Calc/cal.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, ZegopTest) {
+	EXPECT_EQ(4, getZegop(2));
 }
